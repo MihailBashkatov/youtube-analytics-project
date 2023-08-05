4b2ea54ea5e9922a36d4651faa484122be9c8491
Mihail <mihail.bashkatov@gmail.com>
2023-08-05 16:50:29 +0200

    Added attributes for instance in case of initialising: self.title, self.url, self.subscriber_count, self.video_count, self.view_count. Added getter for channel_id. Added class_method get_service. Added method to_json

--- src/channel.py
+++ src/channel.py
@@ -6,12 +6,22 @@ from googleapiclient.discovery import build
 
 import isodate
 
+
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id: str = channel_id
+        self.__channel_id: str = channel_id
+
+        api_key = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.url = channel['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.view_count = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -19,3 +29,28 @@ class Channel:
         youtube = build('youtube', 'v3', developerKey=api_key)
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @property
+    def channel_id(self) -> str:
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        """ Возвращает объект для работы с YouTube API"""
+        api_key = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, file_name):
+        """ Сохраняет в файл значения атрибутов экземпляра `Channel`"""
+
+        dict_attributes = {
+                           'channel_id': self.__channel_id,
+                           'title': self.title,
+                           'url': self.url,
+                           'subscriber_count': self.subscriber_count,
+                           'video_count': self.video_count,
+                           'view_count': self.view_count,
+                          }
+        with open(file_name, 'w') as outfile:
+            json.dump(dict_attributes, outfile, indent=4, ensure_ascii=False)
